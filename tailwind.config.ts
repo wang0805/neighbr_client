ca2c39fc3ff7d8d501c45d435035332679d9b042
wang0805 <wh_wang@live.com>
2025-04-10 20:33:11 +0800

    boiler plate added

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,14 @@
 import type { Config } from "tailwindcss";
 
-
 const config: Config = {
-    darkMode: ["class"],
-    content: [],
+  darkMode: ["class"],
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
   	extend: {
-  		borderRadius: {
-  			lg: 'var(--radius)',
-  			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
-  		},
   		colors: {
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
@@ -23,12 +21,30 @@ const config: Config = {
   				foreground: 'hsl(var(--popover-foreground))'
   			},
   			primary: {
-  				DEFAULT: 'hsl(var(--primary))',
-  				foreground: 'hsl(var(--primary-foreground))'
+  				'50': '#fcfcfc',
+  				'100': '#f1f1f2',
+  				'200': '#e0e0e2',
+  				'300': '#c7c7cc',
+  				'400': '#a8a8af',
+  				'500': '#82828b',
+  				'600': '#57575f',
+  				'700': '#27272a',
+  				'800': '#111113',
+  				'900': '#040405',
+  				'950': '#000000'
   			},
   			secondary: {
-  				DEFAULT: 'hsl(var(--secondary))',
-  				foreground: 'hsl(var(--secondary-foreground))'
+  				'50': '#fefcfc',
+  				'100': '#fdf2f2',
+  				'200': '#fae1e1',
+  				'300': '#f6c9c9',
+  				'400': '#f1abab',
+  				'500': '#eb8686',
+  				'600': '#e45a5a',
+  				'700': '#dc2828',
+  				'800': '#7c1414',
+  				'900': '#400a0a',
+  				'950': '#2c0707'
   			},
   			muted: {
   				DEFAULT: 'hsl(var(--muted))',
@@ -66,6 +82,5 @@ const config: Config = {
   	}
   },
   plugins: [require("tailwindcss-animate")],
-}
-
+};
 export default config;
